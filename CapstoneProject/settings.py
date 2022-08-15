3cdc4aea63b2a3ab4e23719e4400e12cb74fb354
Nirmal Patel <82512930+NirmalPatel17@users.noreply.github.com>
2022-08-15 08:57:55 +0530

    Update settings.py

--- CapstoneProject/settings.py
+++ CapstoneProject/settings.py
@@ -157,7 +157,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ndminer17@gmail.com'
-EMAIL_HOST_PASSWORD = 'xpligswosaywgzty'
+EMAIL_HOST_PASSWORD = ''
 
 
 # sending otp
